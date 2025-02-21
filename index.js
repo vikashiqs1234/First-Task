125e324c5e1ef1626032401257a086f8f7478f5d
Vikash Mishra <vikash@iquincesoft.com>
2025-02-21 13:01:16 +0530

    third commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const ham = document.querySelector('.ham');
 const mobile = document.querySelector('.mobile-nav-ul');
 const cross = document.querySelector('.cross')
+let prevBtn = document.querySelector('.prev-btn')
+let nextBtn = document.querySelector('.next-btn')
+let testoCards = document.querySelector('.testimonial-cards')
 
 const handleHam = ()=>{
     const isMobileVisible = mobile.style.display === "block";
@@ -13,12 +16,26 @@ const handleHam = ()=>{
     cross.style.display = isMobileVisible ? "none" : "block";
 
 }
+const handleScrollPrev = ()=>{
+    testoCards.scrollBy({
+        left: -300,
+        behavior: "smooth"
+    });
+}
+const handleScrollNext = ()=>{
+    testoCards.scrollBy({
+        left: 300,
+        behavior: "smooth"
+    });
+}
 if (window.matchMedia("(max-width: 768px)").matches) {
     console.log("This is a mobile screen.");
 }
 if (ham && cross) {
     ham.addEventListener("click", handleHam);
     cross.addEventListener("click", handleHam);
+    nextBtn.addEventListener('click',handleScrollNext)
+    prevBtn.addEventListener('click',handleScrollPrev)
 }
 
 
